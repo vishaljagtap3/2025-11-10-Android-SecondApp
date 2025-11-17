d5f51688f07cec29f0f0d898db42dd1e3a0e7f50
vishal <vishal.jagtap@bitcode.in>
2025-11-17 12:07:20 +0530

    Added view dynamically

--- app/src/main/java/com/aavidsoft/secondapp/MainActivity.java
+++ app/src/main/java/com/aavidsoft/secondapp/MainActivity.java
@@ -3,17 +3,22 @@ package com.aavidsoft.secondapp;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 
 public class MainActivity extends Activity {
-
     private TextView txt;
     private EditText edt;
     private Button btn;
+    private ImageView img;
+    private Button btnAddView;
+    private LinearLayout mainContainer;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -23,14 +28,60 @@ public class MainActivity extends Activity {
         txt = findViewById(R.id.txt);
         edt = findViewById(R.id.edt);
         btn = findViewById(R.id.btn);
+        img = findViewById(R.id.img);
+        btnAddView = findViewById(R.id.btnAddView);
+        mainContainer = findViewById(R.id.mainContainer);
 
-        btn.setOnClickListener(new BtnClickListener());
+        btnAddView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Button btnNew = new Button(MainActivity.this);
+                btnNew.setLayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT));
+                btnNew.setText("New");
+                mainContainer.addView(btnNew);
+            }
+        });
 
+        /*btn.setOnClickListener(new BtnClickListener());
+        txt.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        txt.setText("");
+                    }
+                }
+        );*/
+
+        MyViewOnClickListener myViewOnClickListener =
+                new MyViewOnClickListener();
+        btn.setOnClickListener(myViewOnClickListener);
+        txt.setOnClickListener(myViewOnClickListener);
+
+        img.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        img.setImageResource(R.drawable.bitcode);
+                    }
+                }
+        );
+    }
+
+    private class MyViewOnClickListener implements View.OnClickListener {
+        @Override
+        public void onClick(View view) {
+            if(view == btn) {
+                txt.setText(edt.getText());
+            }
+            if(view == txt) {
+                txt.setText("");
+            }
+        }
     }
 
     private class BtnClickListener implements View.OnClickListener {
         @Override
-        public void onClick(View v) {
+        public void onClick(View view) {
             txt.setText(edt.getText().toString());
         }
     }
